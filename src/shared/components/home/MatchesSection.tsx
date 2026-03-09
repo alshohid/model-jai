18dac81cb80acc0f1cba5e3a78b2a709f3fddc35
alshohid <alshohid1997@gmail.com>
2026-03-09 16:35:33 +0600

    updated

--- src/shared/components/home/MatchesSection.tsx
+++ src/shared/components/home/MatchesSection.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
-import { useMemo, useState } from "react";
+import { useMemo, useState, useEffect } from "react";
 import { useGetAllPublicMatchListQuery } from "@/redux/features/match/matchManagement";
 import MatchHistoryTabs, { TabKey } from "./MatchHistoryTabs";
 import MatchesGrid from "../grid/MatchesGrid";
+import { formateDate, formateTime } from "@/shared/lib/utils/dateFormater";
 import Image from "next/image";
 import SectionHeading from "./SectionHeading";
 
@@ -24,9 +25,10 @@ const MatchesSection = () => {
 
     const meta = data?.meta ?? {};
 
+    // Filter matches based on tab selection
     const filtered = useMemo(() => {
         if (tab === "all") return matches;
-        return matches.filter((m: any) => m.status === tab);
+        return matches.filter((m: any) => m.type === tab);
     }, [matches, tab]);
 
     return (
