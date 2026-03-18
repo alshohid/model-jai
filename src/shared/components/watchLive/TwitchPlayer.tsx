54fd0e8e4bff8d84e7dc55123b550273fd433dfc
alshohid <alshohid1997@gmail.com>
2026-03-18 10:03:56 +0600

    updated

--- src/shared/components/watchLive/TwitchPlayer.tsx
+++ src/shared/components/watchLive/TwitchPlayer.tsx
@@ -16,7 +16,7 @@ export default function TwitchPlayer() {
             const embed = new Twitch.Embed("twitch-embed", {
                 width: "100%",
                 height: 450,
-                channel: "mattp1tommy",
+                channel: "theburntpeanut",
                 autoplay: true,
                 muted: false,
                 layout: "video",
