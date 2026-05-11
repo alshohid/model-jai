0ead0e0b775c45d307545694a54f15bb24752e3a
alshohid <alshohid1997@gmail.com>
2026-05-11 10:53:18 +0600

    add

--- src/shared/components/watchLive/TwitchPlayer.tsx
+++ src/shared/components/watchLive/TwitchPlayer.tsx
@@ -16,7 +16,8 @@ export default function TwitchPlayer() {
             const embed = new Twitch.Embed("twitch-embed", {
                 width: "100%",
                 height: 450,
-                channel: "modelbossoffers",
+                // channel: "modelbossoffers",
+                channel: "eggsterr",
                 autoplay: true,
                 muted: false,
                 layout: "video",
