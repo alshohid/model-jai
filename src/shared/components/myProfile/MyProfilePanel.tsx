31e5388384eba61a0285b56cdeb3065899474589
alshohid <alshohid1997@gmail.com>
2026-06-17 15:36:55 +0600

    updated

--- src/shared/components/myProfile/MyProfilePanel.tsx
+++ src/shared/components/myProfile/MyProfilePanel.tsx
@@ -181,8 +181,8 @@ export default function MyProfilePanel({
                                     isVisible={visibility?.show_email}
                                     onToggleVisibility={visibilityToggles?.onToggleEmailVisibility}
                                 />
-                                {profile?.contact && <InfoRow label="Contact" value={profile.contact} />}
-                                {profile?.nationality && <InfoRow label="Nationality" value={profile.nationality} />}
+                                {/* {profile?.contact && <InfoRow label="Contact" value={profile.contact} />}
+                                {profile?.nationality && <InfoRow label="Nationality" value={profile.nationality} />} */}
                             </div>
 
                             {game?.name || canChangeFavoriteGame ? (
